48ada3c5951a0f1b9f5d29e1e6fa7ce2f3c36ff5
Pramod Dutta <pramoddutta_96@yahoo.com>
2016-01-04 00:51:02 +0530

    adding template

---
+++ template_java.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	// Project Euler Template for Java
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int[] input_arr = new int[sc.nextInt()];
+
+		for (int i = 0; i < input_arr.length; i++) {
+			input_arr[i] = sc.nextInt();
+
+		}
+		for (int i = 0; i < input_arr.length; i++) {
+			System.out.println(help_function(input_arr[i]));
+		}
+
+	}
+
+	public static int help_function(int n) {
+		
+		
+
+		return 0;
+
+	}
+
+}
